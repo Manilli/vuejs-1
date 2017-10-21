a07f6e48d38d92c0224982ecd199c963adbabe26
neyder.daza <leodaza1995@gmail.com>
2017-10-20 20:29:39 -0500

    final

---
+++ axios/vue2.js
@@ -0,0 +1,18 @@
+
+const vm=new Vue({
+  el:'main',
+  mounted(){
+    this.cargarPersonas();
+  },
+  data:{
+    personas:[]
+  },
+  methods:{
+    cargarPersonas(){
+      axios.get('https://randomuser.me/api/?results=500')
+        .then((respuesta) => {
+          this.personas = respuesta.data.results;
+        });
+    }
+  },
+});
